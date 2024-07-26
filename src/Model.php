01df703d3007ab23afd378ba9fef91ccd8cee228
GhianCo <ghiancorx@gmail.com>
2024-07-26 12:33:28 -0500

    cambios para registtro de datos

--- src/Model.php
+++ src/Model.php
@@ -2,6 +2,8 @@
 
 namespace SophyDB;
 
+use Sophy\Exceptions\ConexionDBException;
+
 class Model
 {
     protected $table;
@@ -25,9 +27,53 @@ class Model
         return (new static)->makeInstance($name, $arguments);
     }
 
-
     public function __call($name, $arguments)
     {
         return $this->makeInstance($name, $arguments);
+    }
+
+    public static function create(array $attributes)
+    {
+        return (new static())->massAsign($attributes);
+    }
+
+    public static function update(array $attributes, int $id)
+    {
+        return (new static())->massAsign($attributes, $id);
+    }
+
+    protected function massAsign(array $attributes, $id = null)
+    {
+        if (count($this->fillable) == 0) {
+            throw new \Error("Entidad " . static::class . " no tiene atributos por asignar");
+        }
+
+        if (isset($id)) {
+            $this->attributes[$this->primaryKey] = $id;
+        }
+
+        foreach ($attributes as $key => $value) {
+            if (in_array($key, $this->fillable)) {
+                $this->attributes[$key] = $value;
+            }
+        }
+
+        return $this;
+    }
+
+    public function save()
+    {
+        try {
+            if (isset($this->attributes[$this->primaryKey])) {
+                $id = $this->attributes[$this->primaryKey];
+                $this->where($this->primaryKey, $id)->update($this->attributes, $id);
+            } else {
+                $id = $this->insertGetId($this->attributes);
+                $this->attributes[$this->primaryKey] = $id;
+            }
+            return $this->attributes;
+        } catch (\Exception $exception) {
+            throw ConexionDBException::showMessage($exception->getMessage());
+        }
     }
 }
