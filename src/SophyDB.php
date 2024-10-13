0ffa84dec53593b74b7948fb7354c9838218290d
GhianCo <ghiancorx@gmail.com>
2024-10-13 12:36:38 -0500

    commit roolback transaction

--- src/SophyDB.php
+++ src/SophyDB.php
@@ -11,12 +11,12 @@ final class SophyDB
 {
     public IDBDriver $database;
 
-    protected static $DB_DEFAULT = IDBDriver::class;
+    protected static $CONN_DEFAULT = IDBDriver::class;
 
     public static function table($name)
     {
         $dml = new DML;
-        $dml->setConnection(app(self::$DB_DEFAULT));
+        $dml->setConnection(app(self::$CONN_DEFAULT));
         $dml->setTable($name);
         return $dml;
     }
@@ -35,16 +35,37 @@ final class SophyDB
         });
     }
 
+    private static function getCurrentConn()
+    {
+        return app(self::$CONN_DEFAULT);
+    }
+
     public static function use(string $config_name)
     {
-        self::$DB_DEFAULT = $config_name;
+        self::$CONN_DEFAULT = $config_name;
         return new static;
+    }
+
+    public static function beginTransaction()
+    {
+        self::getCurrentConn()->connect();
+        self::getCurrentConn()->pdo()->beginTransaction();
+    }
+
+    public static function rollBack()
+    {
+        self::getCurrentConn()->pdo()->rollBack();
+    }
+
+    public static function commit()
+    {
+        self::getCurrentConn()->pdo()->commit();
     }
 
     public static function query($sql, $params = [], $isList = false)
     {
         $dml = new DML;
-        $dml->setConnection(app(self::$DB_DEFAULT));
+        $dml->setConnection(app(self::$CONN_DEFAULT));
         return $dml->execute($sql, $params, true, $isList);
     }
 }
